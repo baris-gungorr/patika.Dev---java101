cb1854c2712d56007977c7b1364caaa594028b3c
Barış Güngör <121685398+baris-gungorr@users.noreply.github.com>
2023-03-28 13:41:30 +0300

    notOrtalamaHesaplama.java

---
+++ src/notOrtalamaHesaplama.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+   int matematik,fizik,kimya,turkce,tarih,müzik;
+
+     Scanner input = new Scanner(System.in);
+
+        System.out.println("Matematik notunu giriniz");
+        matematik = input.nextInt();
+        System.out.println("Matematik notu: " + matematik);
+
+        System.out.println("Fizik notunu giriniz");
+        fizik = input.nextInt();
+        System.out.println("Fizik notu: " +fizik);
+
+        System.out.println("Kimya notunu giriniz");
+        kimya = input.nextInt();
+        System.out.println("Kimya notunuz: " +kimya);
+
+        System.out.println("Türkçe notunu giriniz");
+        turkce = input.nextInt();
+        System.out.println("Türkçe notunuz: "+turkce);
+
+        System.out.println("Tarih notunu giriniz");
+        tarih = input.nextInt();
+        System.out.println("Tarih notunuz: "+tarih);
+
+        System.out.println("Müzik notunu giriniz");
+        müzik = input.nextInt();
+        System.out.println("Müzik notunuz: " +müzik);
+
+        double ortalama = (matematik+fizik+kimya+turkce+tarih+müzik) /6.0;
+
+        System.out.println("Not ortalamanız: " + ortalama);
+
+
+        System.out.println(ortalama >= 60 ? "Kaldınız" : "Geçtiniz");
+    }
+}
