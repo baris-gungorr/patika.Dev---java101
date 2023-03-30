4c9821ba7db8eb9bf68a4696c6385e059fa2eb16
Barış Güngör <121685398+baris-gungorr@users.noreply.github.com>
2023-03-30 18:02:26 +0300

    Activity Suggestion.java

--- src/Activity Suggestion.java
+++ src/Activity Suggestion.java
@@ -2,24 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+       
         Scanner sca = new Scanner(System.in);
         System.out.println("><>< Activity Suggestion ><>< ");
+        
         int heat;
         System.out.println("Hava sıcaklığını giriniz.");
         heat = sca.nextInt();
-
+        
         if (heat <= 5) {
-            System.out.println("Kayak etkinliği");
-        } else if (heat <= 25) {
-            if (heat <= 15) {
-                System.out.println("Sinemaya gidebilirsiniz");
-            }
-                if (heat >= 10) {
-                    System.out.println("Pikniğe gidebilirsiniz");
-
-                } else {
-                    System.out.println("Yüzmeye gidebilirsiniz");
-                }
-            }
+            System.out.println("Kayak yapabilirsiniz");
+       
+        } else if (heat<10 &&heat>=5) {
+            System.out.println("Sinemaya gidebilirsiniz");
+        
+        } else if (heat >=10 && heat<= 15) {
+            System.out.println("Sinema ve pikniğe gidebilirsiniz");
+        
+        } else if (heat >=15 && heat<=20) {
+            System.out.println("Pikniğe gidebilirsiniz");
+        
+        } else {
+            System.out.println("Yüzmeye gidebilirsiniz");
         }
+    }
     }
