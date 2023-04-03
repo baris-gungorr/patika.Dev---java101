6c77ff410949fa5d53a3f9fd8fbd90ace6c04e87
Barış Güngör <121685398+baris-gungorr@users.noreply.github.com>
2023-04-03 14:52:46 +0300

    findEvenNumber.java

---
+++ findEvenNumber.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    
+      int j ;
+      Scanner sca = new Scanner(System.in);
+        System.out.print("Sayı giriniz");
+        j = sca.nextInt();
+
+        int i = 0;
+        while (i<=j) {
+            if (i %2 == 0) {
+                System.out.print(" " +i);
+            }
+            i++;
+        }
+
+        // ÖDEV
+       /* j = sca.nextInt();
+        int i = 0;
+        while (i<=j) {
+            if (i % 3 ==0 && i % 4 == 0)  {
+
+                System.out.print(i+" ");
+                }
+                i++;
+            } */
+        }
+    }
